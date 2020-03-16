c37202c439509faaa287b4c8b487a055ef0fd2dc
Alexander Tolstikov <atolstikov@adyax.com>
2020-03-16 15:37:00 +0300

    Update exec logs

--- includes/exec.sh
+++ includes/exec.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 TOOLBOX_EXEC_LOG_PREFIX=${TOOLBOX_EXEC_LOG_PREFIX-"---> "}
+TOOLBOX_EXEC_LOG_LEVEL_TITLE=${TOOLBOX_EXEC_LOG_LEVEL_TITLE-INFO}
+TOOLBOX_EXEC_LOG_LEVEL_CMD=${TOOLBOX_EXEC_LOG_LEVEL_CMD-INFO}
 
 function _toolbox_exec_log() {
   local title
@@ -11,17 +13,24 @@ function _toolbox_exec_log() {
   local args
   args=$(toolbox_util_array_join "$@")
 
+  local _cmd_log_color="${GREEN}"
+  if [ "${TOOLBOX_EXEC_LOG_LEVEL_TITLE}" = "DEBUG" ] || [ "${TOOLBOX_EXEC_LOG_LEVEL_TITLE}" = "TRACE" ]; then
+    _cmd_log_color="${PURPLE}"
+  fi
 
-  _log DEBUG "${BLUE}${TOOLBOX_EXEC_LOG_PREFIX}${title}:${RESTORE}"
-  _log DEBUG "${GREEN}${args}${RESTORE}"
+  _log "${TOOLBOX_EXEC_LOG_LEVEL_TITLE}" "${BLUE}${TOOLBOX_EXEC_LOG_PREFIX}${title}:${RESTORE}"
+  _log "${TOOLBOX_EXEC_LOG_LEVEL_CMD}" "${_cmd_log_color}${args}${RESTORE}"
 }
 
 function toolbox_exec {
-  _toolbox_exec_log "Exec" "$@"
+  _toolbox_exec_log "$@"
+  shift
   exec "$@"
 }
 
 function toolbox_run {
-  _toolbox_exec_log "Run" "$@"
+  _toolbox_exec_log "$@"
+  shift
   "$@"
 }
+
