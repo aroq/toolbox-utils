2baefb67d6c2da3592a00ae4afc9491ad6287f34
Alexander Tolstikov <atolstikov@adyax.com>
2020-03-18 21:13:42 +0300

    Allow var file to be empty

--- includes/util.sh
+++ includes/util.sh
@@ -7,5 +7,8 @@ function toolbox_util_array_join() {
 function toolbox_util_read_var_from_env_file() {
    VAR=$(grep -w "$1" "$2" | xargs)
    IFS="=" read -ra VAR <<< "$VAR"
+   set +u
    echo "${VAR[1]}"
+   set -u
 }
+
